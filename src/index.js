86ce3afdc61e627eaa596126070e642deeac90d1
WDurangoZapa <wdurangozapa@correo.unicordoba.edu.co>
2019-05-16 20:14:45 +0000

    cambio en la ruta origen

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hola mundo");
+  res.send("Bienvenidos a nuestra aplicación");
 });
 
 app.get("/users", (req, res) => {
